9e8b149b02db04d48a6ad62ccab184229de8282b
Bob Benjamin <benjaminbob500@gmail.com>
2025-11-18 10:54:59 +0300

    addconstapikeynurl

--- weather.js
+++ weather.js
@@ -1,2 +1,3 @@
-const apiKey = "";
-const apiUrl = "";
+const apiKey = "c0c77c983f0c6fcb7486e32fceab077b";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=metric";
